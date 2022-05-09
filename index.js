9272ddf4a4b6e615b43d8cb12729e44ae5fc9062
rabiulhossen <rhsimon69@gmail.com>
2022-05-09 16:44:13 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 const app = express();
 
@@ -14,6 +16,23 @@ app.get('/', (req, res) => {
 });
 
 
+const uri = "mongodb+srv://Rabiulhossen:<password>@cluster0.ea2zl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run (){
+try{
+await client.connect();
+const userCollection = client.db("warehouse").collection("user")
+}
+
+finally{
+// await client.close();
+}
+
+
+
+}
+
+run().catch(console.dir);
 app.listen(port, () => {
     console.log('Listening to port', port);
 })
